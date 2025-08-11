f9df6442023714349f224c2fef45ceff4429d952
Kunal Keshri <sarggek@gmail.com>
2025-08-11 12:59:35 +0530

    Update page1.js

--- page1.js
+++ page1.js
@@ -1,1 +1,10 @@
+// ===== SHOW DETAILS FUNCTION =====
+function showDetails() {
+  document.getElementById("detailsPanel").style.display = "flex"; // Panel visible
+}
+
+// ===== CLOSE DETAILS FUNCTION =====
+function closeDetails() {
+  document.getElementById("detailsPanel").style.display = "none"; // Panel hide
+}
 
